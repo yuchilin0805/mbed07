993df83ffce8e8bcf9776e8e68f2a5eabd4a5031
yuchilin <dog0805@gmail.com>
2020-04-18 22:40:04 +0800

    2

--- src/model_deploy/config.h
+++ src/model_deploy/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H_
 
 // The number of labels (without negative)
-#define label_num 2
+#define label_num 3
 
 struct Config {
 
@@ -29,7 +29,16 @@ struct Config {
         "    *            \n\r"
         "   *             \n\r"
         "  *              \n\r"
-        " * * * * * * * * \n\r"};
+        " * * * * * * * * \n\r"
+        "ONE:\n\r"
+        "       *         \n\r"
+        "       *         \n\r"
+        "       *         \n\r"
+        "       *         \n\r"
+        "       *         \n\r"
+        "       *         \n\r"
+        "       *         \n\r"       
+  };
 };
 
 Config config;
